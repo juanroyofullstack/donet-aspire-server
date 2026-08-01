d57a73592d72f284662a60f7824acbb09ef78d6f
juanroyo <juanroyofullstack@gmail.com>
2026-08-01 18:24:56 +0200

    feat: add configuration files and in-memory settings for application

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,4 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+{
+    ["ASPIRE_ALLOW_UNSECURED_TRANSPORT"] = "true"
+});
 
 builder.AddProject<Projects.NetAspireServer_Api>("api");
 
